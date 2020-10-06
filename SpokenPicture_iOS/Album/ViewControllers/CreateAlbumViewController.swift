3317ed9c0df2b4fea1a9cc9f2a52135ffa756ddb
khaoula.hafsia <khaoula.hafsia@proxym-it.com>
2020-10-06 11:05:34 +0100

    adding album pages with layout1

--- SpokenPicture_iOS/Album/ViewControllers/CreateAlbumViewController.swift
+++ SpokenPicture_iOS/Album/ViewControllers/CreateAlbumViewController.swift
@@ -15,6 +15,7 @@ import Differentiator
 class CreateAlbumViewController: UIViewController {
     
     @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var addPageButton: UIButton!
     
     @IBOutlet weak var albumContentTableView: UITableView!
     
@@ -26,7 +27,6 @@ class CreateAlbumViewController: UIViewController {
         saveButton.roundEdges()
         addNavigationBarItems()
         setUpTableView()
-        setUpBinding()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -34,94 +34,8 @@ class CreateAlbumViewController: UIViewController {
         albumContentTableView.reloadData()
     }
     
-    func setUpBinding(){
-        
-        let initialState = AlbumViewModel(sections: albumsVM.sections)
-        let add3ItemsAddStart = Observable.of((), (), ())
-        
-        let addCommand = Observable.of(saveButton.rx.tap.asObservable(), add3ItemsAddStart)
-            .merge()
-            .map(TableViewEditingCommand.addRandomItem)
-        
-        let deleteCommand = albumContentTableView.rx.itemDeleted.asObservable()
-            .map(TableViewEditingCommand.DeleteItem)
-        //
-        //        Observable.of(addCommand, deleteCommand)
-        //        .merge()
-        //        .scan(initialState) { (state: AlbumViewModel, command: TableViewEditingCommand) -> AlbumViewModel in
-        //            return state.execute(command: command)
-        //        }
-        //        .startWith(initialState)
-        //        .map {
-        //            $0.sections
-        //        }
-        //        .share(replay: 1)
-        //        .bind(to: tableView.rx.items(dataSource: dataSource))
-        //        .disposed(by: disposeBag)
-        
-        let dataSource = RxTableViewSectionedReloadDataSource<AlbumSectionItem>(
-            configureCell: { dataSource, tableView, indexPath, item in
-                switch item.type{
-                case .cover:
-                    if let cell = tableView.dequeueReusableCell(withIdentifier: "AlbumCoverCell", for: indexPath) as? AlbumCoverCell {
-                        cell.setSourceVC(sourceVC: self)
-                        cell.item = item as? AlbumCoverViewModelItem
-                        return cell
-                    }
-                case .pages:
-                    // TO Do fix with approriate Layout here
-                    let albumPages = item as! AlbumPagesViewModelItem
-                    let cellLayout = albumPages.albumPage?.pageLayout
-                    switch cellLayout
-                    {
-                    case .layout1:
-                        let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout1TableViewCell", for: indexPath) as! PageLayout1TableViewCell
-                        cell.setSourceVC(sourceVC: self)
-                        cell.item = item as? AlbumPagesViewModelItem
-                        return cell
-                        
-                    case .layout2:
-                        let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout2TableViewCell", for: indexPath) as! PageLayout2TableViewCell
-                        cell.item = item as? AlbumPagesViewModelItem
-                        cell.setSourceVC(sourceVC: self)
-                        return cell
-                    case .layout3:
-                        let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout3TableViewCell", for: indexPath) as! PageLayout3TableViewCell
-                        cell.setSourceVC(sourceVC: self)
-                        cell.item = item as? AlbumPagesViewModelItem
-                        return cell
-                    case .layout4:
-                        let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout4TableViewCell", for: indexPath) as! PageLayout4TableViewCell
-                        cell.setSourceVC(sourceVC: self)
-                        cell.item = item as? AlbumPagesViewModelItem
-                        return cell
-                    case .layout5:
-                        let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout5TableViewCell", for: indexPath) as! PageLayout5TableViewCell
-                        cell.setSourceVC(sourceVC: self)
-                        cell.item = item as? AlbumPagesViewModelItem
-                        return cell
-                    case .layout6:
-                        let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout6TableViewCell", for: indexPath) as! PageLayout6TableViewCell
-                        cell.setSourceVC(sourceVC: self)
-                        cell.item = item as? AlbumPagesViewModelItem
-                        return cell
-                    case .none:
-                        return UITableViewCell()
-                    }
-                    
-                }
-                // return the default cell if none of above succeed
-                return UITableViewCell()
-        })
-        
-        
-        Observable.just(albumsVM.sections)
-            .bind(to: albumContentTableView.rx.items(dataSource: dataSource))
-            .disposed(by: disposeBag)
-    }
-    
     func setUpTableView(){
-        
+        albumContentTableView.dataSource = self
         albumContentTableView.registerCell(nib: "PageLayout1TableViewCell", cellreuseIdentifier: "PageLayout1TableViewCell")
         albumContentTableView.registerCell(nib: "PageLayout2TableViewCell", cellreuseIdentifier: "PageLayout2TableViewCell")
         albumContentTableView.registerCell(nib: "PageLayout3TableViewCell", cellreuseIdentifier: "PageLayout3TableViewCell")
@@ -150,8 +64,7 @@ class CreateAlbumViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func addPageButtonClicked(_ sender: Any){
-        
+    func showAddPagePopUp(){
         let popUp = album.instantiateViewController(withIdentifier: "ChooseLayoutViewController") as! ChooseLayoutViewController
         //        popUp.createAlbumVM = self.createAlbumsVM
         self.addChild(popUp)
@@ -160,40 +73,18 @@ class CreateAlbumViewController: UIViewController {
         popUp.didMove(toParent: self)
     }
     
-    @IBAction func saveButtonClicked(_ sender: Any) {
+    @IBAction func addPageButtonClicked(_ sender: Any){
+        
+        let pages = AlbumPagesViewModelItem()
+        let page = AlbumPage(pageLayout: .layout1 , pageNumber: albumsVM.sections.count, numberPictures: 1)
+        pages.albumPage = page
+        self.albumsVM.sections.append(
+            AlbumSectionItem(header: "Second section", items: [pages]))
+        self.albumContentTableView.reloadData()
+        
     }
-}
-
-//extension CreateAlbumViewController {
-//    static func dataSource() -> RxTableViewSectionedAnimatedDataSource<AlbumSectionItem> {
-//        return RxTableViewSectionedAnimatedDataSource(
-//            animationConfiguration: AnimationConfiguration(insertAnimation: .top,
-//                                                                   reloadAnimation: .fade,
-//                                                                   deleteAnimation: .left),
-//            configureCell: { _, table, idxPath, item in
-//                let cell = table.dequeueReusableCell(withIdentifier: "Cell", for: idxPath)
-//                cell.textLabel?.text = "\(item)"
-//                return cell
-//            },
-//            titleForHeaderInSection: { ds, section -> String? in
-//                return ds[section].header
-//            },
-//            canEditRowAtIndexPath: { _, _ in
-//                return true
-//            },
-//            canMoveRowAtIndexPath: { _, _ in
-//                return true
-//            }
-//        )
-//    }
-//}
-
-extension TableViewEditingCommand {
-    static func addRandomItem() -> TableViewEditingCommand {
-        let page = AlbumPagesViewModelItem()
-        page.albumPage = AlbumPage(pageLayout: .layout5, pageNumber: 2, numberPictures: 3)
-        let item = AlbumSectionItem(header: "", items: [page])
-        return TableViewEditingCommand.AppendItem(item: item, section: 2)
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
     }
 }
 
@@ -201,5 +92,71 @@ extension CreateAlbumViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 300
     }
+}
+
+extension CreateAlbumViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return albumsVM.sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let page = albumsVM.sections[indexPath.section]
+        let item = page.items[indexPath.row]
+        switch item.type    {
+        case .cover:
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "AlbumCoverCell", for: indexPath) as? AlbumCoverCell {
+                cell.setSourceVC(sourceVC: self)
+                cell.item = item as? AlbumCoverViewModelItem
+                return cell
+            }
+        case .pages:
+            // TO Do fix with approriate Layout here
+            let albumPages = item as! AlbumPagesViewModelItem
+            let cellLayout = albumPages.albumPage?.pageLayout
+            switch cellLayout
+            {
+            case .layout1:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout1TableViewCell", for: indexPath) as! PageLayout1TableViewCell
+                cell.setSourceVC(sourceVC: self)
+                cell.item = item as? AlbumPagesViewModelItem
+                return cell
+                
+            case .layout2:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout2TableViewCell", for: indexPath) as! PageLayout2TableViewCell
+                cell.item = item as? AlbumPagesViewModelItem
+                cell.setSourceVC(sourceVC: self)
+                return cell
+            case .layout3:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout3TableViewCell", for: indexPath) as! PageLayout3TableViewCell
+                cell.setSourceVC(sourceVC: self)
+                cell.item = item as? AlbumPagesViewModelItem
+                return cell
+            case .layout4:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout4TableViewCell", for: indexPath) as! PageLayout4TableViewCell
+                cell.setSourceVC(sourceVC: self)
+                cell.item = item as? AlbumPagesViewModelItem
+                return cell
+            case .layout5:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout5TableViewCell", for: indexPath) as! PageLayout5TableViewCell
+                cell.setSourceVC(sourceVC: self)
+                cell.item = item as? AlbumPagesViewModelItem
+                return cell
+            case .layout6:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "PageLayout6TableViewCell", for: indexPath) as! PageLayout6TableViewCell
+                cell.setSourceVC(sourceVC: self)
+                cell.item = item as? AlbumPagesViewModelItem
+                return cell
+            case .none:
+                return UITableViewCell()
+            }
+        }
+        // return the default cell if none of above succeed
+        return UITableViewCell()
+    }
+    
 }
 
--- SpokenPicture_iOS/Album/ViewModels/AlbumViewModel.swift
+++ SpokenPicture_iOS/Album/ViewModels/AlbumViewModel.swift
@@ -23,31 +23,26 @@ class AlbumViewModel{
     
     init(){
         let pages = AlbumPagesViewModelItem()
-        let page = AlbumPage(pageLayout: .layout1 , pageNumber: 1, numberPictures: 3)
+        let page = AlbumPage(pageLayout: .layout1 , pageNumber: 1, numberPictures: 1)
         pages.albumPage = page
-        let pages1 = AlbumPagesViewModelItem()
-        let pagge1 = AlbumPage(pageLayout: .layout2, pageNumber: 2, numberPictures: 2)
-        pages1.albumPage = pagge1
-        let pages2 = AlbumPagesViewModelItem()
-        let page2 = AlbumPage(pageLayout: .layout3 , pageNumber: 3, numberPictures: 3)
-        pages2.albumPage = page2
-        let pages3 = AlbumPagesViewModelItem()
-        let pagge3 = AlbumPage(pageLayout: .layout4, pageNumber: 4, numberPictures: 3)
-        pages3.albumPage = pagge3
-        let pages4 = AlbumPagesViewModelItem()
-        let page4 = AlbumPage(pageLayout: .layout5 , pageNumber: 5, numberPictures: 3)
-        pages4.albumPage = page4
-        let pages5 = AlbumPagesViewModelItem()
-        let pagge5 = AlbumPage(pageLayout: .layout6, pageNumber: 6, numberPictures: 2)
-        pages5.albumPage = pagge5
+//        let pages1 = AlbumPagesViewModelItem()
+//        let pagge1 = AlbumPage(pageLayout: .layout2, pageNumber: 2, numberPictures: 2)
+//        pages1.albumPage = pagge1
+//        let pages2 = AlbumPagesViewModelItem()
+//        let page2 = AlbumPage(pageLayout: .layout3 , pageNumber: 3, numberPictures: 3)
+//        pages2.albumPage = page2
+//        let pages3 = AlbumPagesViewModelItem()
+//        let pagge3 = AlbumPage(pageLayout: .layout4, pageNumber: 4, numberPictures: 3)
+//        pages3.albumPage = pagge3
+//        let pages4 = AlbumPagesViewModelItem()
+//        let page4 = AlbumPage(pageLayout: .layout5 , pageNumber: 5, numberPictures: 3)
+//        pages4.albumPage = page4
+//        let pages5 = AlbumPagesViewModelItem()
+//        let pagge5 = AlbumPage(pageLayout: .layout6, pageNumber: 6, numberPictures: 2)
+//        pages5.albumPage = pagge5
         sections = [
             AlbumSectionItem(header: "First section", items: [AlbumCoverViewModelItem()]),
-            AlbumSectionItem(header: "Second section", items: [pages]),
-            AlbumSectionItem(header: "Second section", items: [pages1]),
-            AlbumSectionItem(header: "Second section", items: [pages2]),
-            AlbumSectionItem(header: "Second section", items: [pages3]),
-            AlbumSectionItem(header: "Second section", items: [pages4]),
-            AlbumSectionItem(header: "Second section", items: [pages5])
+            AlbumSectionItem(header: "Second section", items: [pages])
         ]
     }
     
@@ -59,7 +54,7 @@ class AlbumViewModel{
 //        switch command {
 //        case .AppendItem(let appendEvent):
 //            var sections = self.sections
-//            let items = sections[appendEvent.section].items + appendEvent.item 
+//            let items = sections[appendEvent.section].items + appendEvent.item as! AlbumViewModelItem
 //            sections[appendEvent.section] = AlbumSectionItem(original: sections[appendEvent.section], items: items)
 //            return AlbumViewModel(sections: sections)
 //        case .DeleteItem(let indexPath):
@@ -74,15 +69,15 @@ class AlbumViewModel{
 }
 
 struct AlbumSectionItem {
-    
+
     var header: String
     var items: [AlbumViewModelItem]
-    
-    
+
+
 }
 extension AlbumSectionItem: SectionModelType {
     typealias Item = AlbumViewModelItem
-    
+
     init(original: AlbumSectionItem, items: [Item]) {
         self = original
         self.items = items
